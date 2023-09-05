034f355726d9f39b4f89633d588c7f455e9fea0e
Primey Nonny Mouse <121109964+PrimeyThePenguin@users.noreply.github.com>
2023-09-05 04:30:59 +0000

    Add files via upload

---
+++ important_quiz.py
@@ -0,0 +1,8 @@
+while True:
+    ssn=input("Quick! What's Shadow the Hedgehog's Social Security Number? ").lower()
+
+    if ssn == '69':
+       print('Aww yeah! This is happenin!')
+       quit()
+    else:
+       print('Skill issue! Try again.')
